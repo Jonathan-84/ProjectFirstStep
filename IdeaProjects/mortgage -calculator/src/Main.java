ddf395b1b415328eb5ce0e6a66a33a97e51490b4
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2022-05-14 00:12:20 -0400

    updated with the refactored code

--- IdeaProjects/mortgage -calculator/src/Main.java
+++ IdeaProjects/mortgage -calculator/src/Main.java
@@ -25,55 +25,48 @@ math pow
             System.out.print("name:");
             String myName = name.nextLine();
             System.out.println("My name is " + myName );*/
-final byte Months = 12;
-final byte Percent = 100;
-int myPrincipal = 0;
-int mortgageLength = 0;
-
-float monthlyInt = 0;
-int numberPayments =0;
-
-            Scanner scanner = new Scanner(System.in);
-
-      while(true) {
-          System.out.print("Principal:");
-          myPrincipal = scanner.nextInt();
-          if (myPrincipal >= 1000 && myPrincipal <= 1_000_000)
-              break;
-          System.out.println("Enter a value between 1000 and 1,000,000");
-      }
-
-      while(true) {
-          //annual interest rate ex.3.92
-          System.out.print("Annual Interest:");
-          float interest = scanner.nextFloat();
-          if (interest > 0 && interest <= 30) {
-              monthlyInt = interest / Percent / Months;
-              break;
-      }
-          System.out.println("Enter a value greater than 0 but less than or equal to 30");
-      }
-      while(true) {
-          System.out.print("year term:");
-          mortgageLength = scanner.nextInt();
-          if (mortgageLength > 1 && mortgageLength < 30) {
-              numberPayments = mortgageLength * Months;
-              break;
-          }
-          System.out.println("Enter a value between 1 and 30");
-
-      }
 
 
+int myPrincipal =(int) readNumber( "Principal: ", 1000, 1_000_000);
+float interest =(float) readNumber( "Annual Interest Rate: ", 0, 30);
+byte mortgageLength =(byte) readNumber( "Mortgage Term (Years):", 1, 30);
 
-//onthly cost is off/ the first part of the double mortgage was wrong...
-          double mortgage = myPrincipal * monthlyInt
-                  / (1 - Math.pow(1 + monthlyInt, -numberPayments));
+
+double mortgage = calculateMortgage(myPrincipal, interest, mortgageLength );
+
           // monthly payment
 
           String mortgageForm = NumberFormat.getCurrencyInstance().format(mortgage);
           System.out.println("Monthly Payment: " + mortgageForm + " .");
       }
 
+      public static double readNumber(String prompt, double min, double max ){
+            Scanner scanner=new Scanner(System.in);
+            double value;
+        while(true) {
+            //annual interest rate ex.3.92
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value > min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max + ".");
+        }
+return value;
+    }
+      public static double calculateMortgage (
+              int myPrincipal,
+              float interest,
+              byte mortgageLength ) {
+
+          final byte Months = 12;
+          final byte Percent = 100;
+
+          float monthlyInt = interest / Percent / Months;
+          short numberPayments =(short)(mortgageLength * Months);
+          //onthly cost is off/ the first part of the double mortgage was wrong...
+          double mortgage = myPrincipal * monthlyInt
+                  / (1 - Math.pow(1 + monthlyInt, -numberPayments));
+          return mortgage;
+      }
 
 }
