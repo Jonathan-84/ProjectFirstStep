01091e8c2ad347e82457fff41b6d5d1bde858b48
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2022-05-17 22:24:23 -0400

    finished course 1, includes payments left

--- IdeaProjects/mortgage -calculator/src/Main.java
+++ IdeaProjects/mortgage -calculator/src/Main.java
@@ -3,44 +3,40 @@ import java.util.Scanner;
 
 public class Main {
 
+    final static byte Months = 12;
+    final static  byte Percent = 100;
+
         public static void main(String[] args) {
-
-            /*
-            prncipal, annual interest rate term length
-            Monthly mortgage total
-
-            annuual interest rate... user entry / 100 then /12
-
-math pow 
-            hints: principal * monthly interest rate
-
-            annual interest rate 3.92 example needs to be divided by 12
-             */
-           /* Scanner age = new Scanner(System.in);
-            System.out.print("age:");
-            byte myAge = age.nextByte();
-            System.out.println("I am " + myAge + " years old");
-
-            Scanner name = new Scanner(System.in);
-            System.out.print("name:");
-            String myName = name.nextLine();
-            System.out.println("My name is " + myName );*/
 
 
 int myPrincipal =(int) readNumber( "Principal: ", 1000, 1_000_000);
 float interest =(float) readNumber( "Annual Interest Rate: ", 0, 30);
 byte mortgageLength =(byte) readNumber( "Mortgage Term (Years):", 1, 30);
 
+            printMortgage(myPrincipal, interest, mortgageLength);
+            printPaymentSchedule(myPrincipal, interest, mortgageLength);
+        }
 
-double mortgage = calculateMortgage(myPrincipal, interest, mortgageLength );
+    private static void printMortgage(int myPrincipal, float interest, byte mortgageLength) {
+        double mortgage = calculateMortgage(myPrincipal, interest, mortgageLength);
+        String mortgageForm = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Monthly Payment: " + mortgageForm + " .");
+        System.out.println();
+        System.out.println("MoRTAGE");
+        System.out.println("Mortgage Payments:" + mortgageForm);
+    }
 
-          // monthly payment
+    private static void printPaymentSchedule(int myPrincipal, float interest, byte mortgageLength) {
+        System.out.println();
+        System.out.println("PayMENT SCHEDULE");
+        System.out.println("-----------E");
+        for(short month = 1; month<= mortgageLength * Months; month++ ){
+          double balance = calculateBalance(myPrincipal, interest, mortgageLength, month);
+          System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
 
-          String mortgageForm = NumberFormat.getCurrencyInstance().format(mortgage);
-          System.out.println("Monthly Payment: " + mortgageForm + " .");
-      }
-
-      public static double readNumber(String prompt, double min, double max ){
+    public static double readNumber(String prompt, double min, double max ){
             Scanner scanner=new Scanner(System.in);
             double value;
         while(true) {
@@ -53,13 +49,25 @@ double mortgage = calculateMortgage(myPrincipal, interest, mortgageLength );
         }
 return value;
     }
+
+    public static double calculateBalance (
+            int myPrincipal,
+            float interest,
+            byte mortgageLength,
+            short numberOfPaymentsMade
+    ){
+
+        float monthlyInt = interest / Percent / Months;
+        short numberPayments =(short)(mortgageLength * Months);
+
+        double balance= myPrincipal * (Math.pow(1+monthlyInt, numberPayments) - Math.pow(1 + monthlyInt, numberOfPaymentsMade))
+                /(Math.pow(1 + monthlyInt, numberPayments)-1);
+        return balance;
+    }
       public static double calculateMortgage (
               int myPrincipal,
               float interest,
               byte mortgageLength ) {
-
-          final byte Months = 12;
-          final byte Percent = 100;
 
           float monthlyInt = interest / Percent / Months;
           short numberPayments =(short)(mortgageLength * Months);
